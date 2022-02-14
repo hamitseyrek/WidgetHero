0f50f0582f9ebe123a55a7c679916fca499dbecf
hamitseyrek <hamitseyrek@gmail.com>
2022-02-14 22:27:55 +0300

    Initial Commit

---
+++ WidgetHero/WidgetHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetHeroApp.swift
+//  WidgetHero
+//
+//  Created by Hamit Seyrek on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WidgetHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
