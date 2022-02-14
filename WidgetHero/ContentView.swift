1a0ed5dbfe7f85d2c4ac036354e714e8a93ddeb2
hamitseyrek <hamitseyrek@gmail.com>
2022-02-15 00:07:48 +0300

    usage AppStorage for save to UserDefaults

--- WidgetHero/ContentView.swift
+++ WidgetHero/ContentView.swift
@@ -10,11 +10,24 @@ import SwiftUI
 let heroArray = [ironMan, spiderMan, hulk, wonderWomen]
 
 struct ContentView: View {
+    
+    @AppStorage ("hero", store : UserDefaults(suiteName: "group.com.hamitseyrek.WidgetHero"))
+    var heroData : Data = Data()
+    
     var body: some View {
         VStack{
             ForEach (heroArray) { hero in
                 HeroView(hero: hero)
+                    .onTapGesture {
+                        saveToUserDefaults(hero: hero)
+                    }
             }
+        }
+    }
+    
+    func saveToUserDefaults(hero : Hero) {
+        if let herodata = try? JSONEncoder().encode(hero) {
+            self.heroData = herodata
         }
     }
 }
